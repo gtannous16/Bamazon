0db30afe21f36142db5e964b30c999fde9c96e39
Gigi Tannous <gtannous16@gmail.com>
2019-05-11 16:42:03 -0400

    added some code to bamazon.js

--- bamazon.js
+++ bamazon.js
@@ -1,5 +1,9 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require('dotenv').config();
 var figlet = require('figlet');
 var chalk = require('chalk');
+
 console.log("\x1b[33m");
 (figlet('Welcome to Bamazon!', function(err, data) {
     if (err) {
@@ -9,3 +13,21 @@ console.log("\x1b[33m");
     }
     console.log(data)
 }));
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+  
+    port: 3306,
+    
+    user: process.env.DB_USER,
+  
+    password: process.env.DB_PASS,
+    database: "Bamazon_DB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+
