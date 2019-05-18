225e95976f47eb3319394f9348d881a7b6cd38a2
Gigi Tannous <gtannous16@gmail.com>
2019-05-17 22:19:23 -0400

    added bamazonCustomer code

--- bamazon.sql
+++ bamazon.sql
@@ -2,7 +2,7 @@ CREATE DATABASE Bamazon_DB;
 
 USE Bamazon_DB;
 
-CREATE TABLE auctions(
+CREATE TABLE Products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(45) NOT NULL,
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@ var inquirer = require("inquirer");
 require('dotenv').config();
 var figlet = require('figlet');
 var chalk = require('chalk');
+var Table = require('cli-table');
 
 console.log("\x1b[33m");
 (figlet('Welcome to Bamazon!', function(err, data) {
@@ -25,9 +26,69 @@ var connection = mysql.createConnection({
     database: "Bamazon_DB"
   });
   
-  connection.connect(function(err) {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
+  connection.connect(function(err){
+  
+    if(err)throw err;
+  
+  var displayProducts = function(){
+    var query = "Select * FROM Products";
+    connection.query(query, function(err, res){
+      if(err) throw err;
+      var displayTable = new Table ({
+        head: ["Item ID", "Product Name", "Department", "Price", "Quantity"],
+        colWidths: [10,35,35,10,14]
+      });
+      for(var i = 0; i < res.length; i++){
+        displayTable.push(
+          [res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+          );
+      }
+      console.log('\n')
+      console.log(displayTable.toString());
+      
+    });
+  }
+  
+  displayProducts();
+  
+  function purchasePrompt(){
+    inquirer.prompt([
+    {
+      name: "ID",
+      type: "input",
+      message:"Please enter the Item ID you would like to purchase.\n",
+      filter:Number
+    },
+    {
+      name:"Quantity",
+      type:"input",
+      message:"How many items would you like to purchase?",
+      filter:Number
+    },
+  
+   ]).then(function(answers){
+     var quantityNeeded = answers.Quantity;
+     var IDrequested = answers.ID;
+    purchaseOrder(IDrequested, quantityNeeded); 
+   });
+  };
+  
+  purchasePrompt();
+  
+  function purchaseOrder(ID, amtNeeded){
+    connection.query('Select * FROM Products WHERE item_id = ' + ID, function(err,res){
+      if(err){console.log(err)};
+      if(amtNeeded <= res[0].stock_quantity){
+        var totalCost = res[0].price * amtNeeded;
+        console.log(chalk.yellow("Good news your order is in stock!"));
+        console.log(chalk.yellow("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you for your Order!"));
+  
+        connection.query("UPDATE Products SET stock_quantity = stock_quantity - " + amtNeeded + " WHERE item_id = " + ID);
+      } else{
+        console.log(chalk.red("Insufficient quantity, I'm sorry we don't have enough " + res[0].product_name + "'s to complete your order. Please purchase a different item or change the amount of the item you need!"));
+      };
+      displayProducts();
+      purchasePrompt();
+    });
+  };
   });
-
